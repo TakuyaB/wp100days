5aa80b2b909fd9b6183507be17562208d618aad3
TakuyaB <takuya.b.works@gmail.com>
2021-10-25 19:46:21 +0900

    Day3

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>タイトルです</title>
+  <title><?php bloginfo(); ?></title>
   <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +13,7 @@
 <body>
   <header>
     <div class="wrapper">
-      ヘッダー
+      <h1><a href="<?php echo home_url(); ?>"><?php bloginfo(); ?></a></h1>
+      <p><?php bloginfo('description'); ?></p>
     </div>
   </header>
