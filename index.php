e780ec604613d7b74e5410a923ed2ce49006a423
TakuyaB <takuya.b.works@gmail.com>
2021-10-31 23:03:21 +0900

    Day4終了時点

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php get_header(); ?>
-  <div class="container">
-    <div class="wrapper flex">
-      <main class="left">メインコンテンツ</main>
-      <?php get_sidebar(); ?>
-    </div>
+<div class="container">
+  <div class="wrapper flex">
+    <main class="left">
+      index.php
+      <div class="articles">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="article">
+              <h1 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              <p class="article-content"><?php the_excerpt(); ?></p>
+            </div>
+        <?php endwhile; else : ?>
+          記事がありません。
+        <?php endif; ?>
+      </div>
+    </main>
+    <?php get_sidebar(); ?>
   </div>
+</div>
 <?php get_footer(); ?>
